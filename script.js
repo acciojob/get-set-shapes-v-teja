25bebd6a6d683edef3e58df37f1d95e22c324465
Viswa Teja <115293958+v-teja@users.noreply.github.com>
2023-04-28 22:47:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this.width = width;
+		this.height = height;
+	}
+	get width{
+	return this.width;
+	}
+	get height{
+	return this.height;
+	}
+	getArea(){
+	return (this.width)*(this.height);
+	}
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	construcor(side){
+		super(side,side);
+	}
+	getPerimeter(){
+		return 4*(this.side);
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
